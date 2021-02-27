83373eb3b50fcd9a32c5781c0895568a6e6541d6
Saurabh Hinduja <ntweat@RIa-Mac.local>
2021-02-26 22:59:55 -0500

    Submission

--- Assignment2_DIS_Spring2021/Program.cs
+++ Assignment2_DIS_Spring2021/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace Assignment2_DIS_Spring2021
 {
     class Program
@@ -7,9 +8,11 @@ namespace Assignment2_DIS_Spring2021
         static void Main(string[] args)
         {
             //Question1:
+            Console.WriteLine("Question 1");
             int[] ar1 = { 2, 5, 1, 3, 4, 7 };
             int n1 = 3;
             ShuffleArray(ar1, n1);
+            Console.WriteLine();
 
             //Question 2 
             Console.WriteLine("Question 2");
@@ -34,6 +37,7 @@ namespace Assignment2_DIS_Spring2021
             Console.WriteLine("Question 5");
             string s5 = "korfsucy";
             int[] indices = { 6, 4, 3, 2, 1, 0, 5, 7 };
+            RestoreString(s5, indices);
             Console.WriteLine();
 
             //Question 6
@@ -111,9 +115,40 @@ namespace Assignment2_DIS_Spring2021
         {
             try
             {
-                //write your code here.
-                
+                if (nums.Length == 0 || nums.Length != 2*n) // Edge case if empty list is passed. 
+                {
+                    Console.WriteLine("Error need atleast 1 element OR number of elements is not equal to 2n");
+                }
+                else
+                {
+                    //write your code here.
+                    int[] result = new int[nums.Length];   // Result Array of length 2n
+
+                    for (int i = 0; i < n; i++)   // Pupulate every even number
+                    {
+                        result[i * 2] = nums[i];
+                    }
+
+                    for (int j = 1; j < n + 1; j++)  //Populare odd numbers
+                    {
+                        result[(j * 2) - 1] = nums[n + j - 1];
+                    }
+
+                    //Print the array
+
+                    Console.Write("[");
+                    for (int i = 0; i < result.Length; i++)
+                    {
+                        Console.Write(result[i]);
+                        if(i<result.Length-1)
+                            Console.Write(",");
+
+                    }
+                    Console.Write("]");
+                }
             }
+
+            //Reflection: I was able to do it in O(2n). The main driving point was to populate odd and even located inigers seperatly. 
             catch (Exception)
             {
 
@@ -134,9 +169,45 @@ namespace Assignment2_DIS_Spring2021
         {
             try
             {
-                //write your code here.
-                
+
+                if (ar2.Length == 0) // Edge case if empty list is passed. 
+                {
+                    Console.WriteLine("Error need atleast 1 element");
+                }
+                else
+                {
+                    //write your code here.
+                    int j = 0;     // Keep track of how many non-zero elements
+                    int z = 0;     //keep track of how many zeros
+
+                    for (int i = 0; i < ar2.Length; i++)  // loop though the array 
+                    { 
+                        if (ar2[i] != 0)    //check if element is not zero
+                        {
+                            ar2[j] = ar2[i]; //Push the non-zero elements
+                            j++;               
+                        }
+                        else
+                        {
+                            z++;    //track if zero
+                        }
+                    }
+                    for (int i = 0; i < z; i++) // loop for all zero elements
+                        ar2[j + i] = 0;      // push zeros to array
+
+                    //print the array
+                    Console.Write("[");
+                    for (int i = 0; i < ar2.Length; i++)
+                    {
+                        Console.Write(ar2[i] + ",");
+                    }
+                    Console.Write("]");
+                }
+
             }
+
+
+            // Reflection : This was one of the simple questions of the assignment. We just had to keep track of the zeros and push the non zero. 
             catch (Exception)
             {
 
@@ -164,8 +235,36 @@ namespace Assignment2_DIS_Spring2021
         {
             try
             {
-                //write your code here.
+
+                if (nums.Length < 2) // Edge case if empty list is passed. 
+                {
+                    Console.WriteLine("Error, need atleast 2 elements");
+                }
+                else
+                {
+                    int count = 0;    // count the cool numbers
+                    //write your code here.
+                    Dictionary<int, int> track = new Dictionary<int, int>();  // Dictionary to keep track of the numbers
+
+                    for (int i = 0; i < nums.Length; i++) // loop
+                    {
+                        if (track.ContainsKey(nums[i]))  //check if number is in dictionary
+                        {
+                            count += track[nums[i]];  // if number found add it to count
+                            track[nums[i]]++;       // increase it coolness
+                        }
+
+                        else
+                        {
+                            track.Add(nums[i], 1);  // add number to dictionary with coolness of 1
+                        }
+                    }
+                    Console.WriteLine(count);   
+                }
             }
+
+
+            // Reflection: Initially I did it in O(n^2) by having 2 nested loops. To solve it in O(n) I again had to use a dictionary to keep track of the numbers seen.
             catch (Exception)
             {
 
@@ -195,7 +294,34 @@ namespace Assignment2_DIS_Spring2021
             try
             {
                 //write your code here.
-                
+
+                if (nums.Length == 0) // Edge case if empty list is passed. 
+                {
+                    Console.WriteLine("Error, need atleast 1 element");
+                }
+                else
+                {
+
+                    Dictionary<int, int> ts = new Dictionary<int, int>(); // Dictionary to keep track of numbers and the differences.
+
+                    for (int i = 0; i < nums.Length; i++)
+                    {
+                        int dif = target - nums[i]; // Calculate the difference between target and current number
+                        if (ts.ContainsKey(dif))    // if difference is there in dictionary 
+                        {
+
+                            Console.WriteLine("[" + ts[dif] + "," + i + "]"); // print the result
+                            break;  // Break the loop as there is only 1 result
+                        }
+                        else
+                        {
+                            ts.Add(nums[i], i);  // add the number and index. 
+                        }
+                    }
+                }
+
+                // Reflection: The challenge was to solve it in O(n). The solution was keep track of index of number and if we find that difference between target and current number is in dictionary.
+
             }
             catch (Exception)
             {
@@ -227,7 +353,20 @@ namespace Assignment2_DIS_Spring2021
             try
             {
                 //write your code here.
-                
+                if (s.Length != indices.Length) // check if lenght of string and array is same
+                {
+                    Console.WriteLine("Error. Indices array and string of different length");
+                }
+                else
+                {
+
+                    char[] str = new char[indices.Length];  // empty character array of length of string
+                    for (int i = 0; i < indices.Length; i++)    // iterate over the indices array
+                    {
+                        str[indices[i]] = s.ElementAt(i);  // store character at the given index
+                    }
+                    Console.WriteLine(new String(str));  // convert the character array to string and console write
+                }
             }
             catch (Exception)
             {
@@ -235,6 +374,8 @@ namespace Assignment2_DIS_Spring2021
                 throw;
             }
         }
+        // Reflections:  The datastructure was the main thing to see. Logic of converting array of characters to string. 
+
 
         //Question 6
         /// <summary>
@@ -259,8 +400,33 @@ namespace Assignment2_DIS_Spring2021
             try
             {
                 //write your code here.
+
+                if (s1.Length!=s2.Length) // Edge case. If lengths of 2 strings are not equal
                 return false;
+
+                Dictionary<char, char> dict = new Dictionary<char, char>(); // initialize dictionary to keep track of characters
+
+
+                for (int i=0; i<s1.Length; i++)    // Loop through the strings
+                {
+                    if (dict.ContainsKey(s1[i]))   // check if character is dictionary
+                    {
+                        if (dict[s1[i]] != s2[i])   // Is the character coded same as the code in dictionary
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        dict.Add(s1[i], s2[i]);  // If character not in dictionary. Add the character and what it is coded as.
+                    }
+                }
+
+
+                return true;
             }
+
+            /// Reflection : The key was keeping a Dictionary keeping track of what character is coded as which character. 
             catch (Exception)
             {
 
@@ -294,13 +460,61 @@ namespace Assignment2_DIS_Spring2021
             try
             {
                 //write your code here.
-                
+
+                Dictionary<int, List<int>> Store = new Dictionary<int, List<int>>(); // Dictionary to keep track of students and their scores. Ref reflections
+
+                for (int i=0; i< items.Length/2; i++) // loop through all the students
+                {
+
+                if (Store.ContainsKey(items[i, 0])) // check if subject in dictionary
+                    {
+                        Store[items[i, 0]].Add(items[i, 1]); // if student in dictionary add this score
+                    }
+                    else // else (if subject not in dictionary)
+                    {
+                        List<int> gg = new List<int>(); // initialise an empty list
+                        gg.Add(items[i, 1]); // add score to list
+                        Store.Add(items[i, 0], new List<int>(gg)); // add student and new list to dictionary 
+                    }
+                }
+
+                // starting output
+                Console.Write("[");
+                for (int i=0; i<Store.Count; i++)
+                {
+                    int ke = Store.ElementAt(i).Key; // get student
+                    int[] aa = Store[ke].ToArray(); // get marks of student
+                    Array.Sort(aa); //Sort the array
+                    Array.Reverse(aa);  // reverse the sorted array
+
+
+                    if (aa.Length < 5) // check if the student has marks for 5 subjects (edge case)
+                    {
+                        Console.Write("[" + ke + "," + "Student does not have 5 subjects" + "]");
+                    }
+                    else
+                    {
+                        int sum = 0;  // initialize sum for student 
+
+                        for (int j = 0; j < 5; j++)
+                        {
+                            sum = sum + aa[j];   // calculate sum of top 5 subject of students 
+                        }
+                        Console.Write("[" + ke + "," + sum / 5 + "]"); // print it the student and the average
+                    }
+                }
+                Console.Write("]");
+
+
+               //Reflection: Datastructure to be used in this question was major thing. I used a Dictionry which had student as the key and marks the student got in the list
+               // Edge case which I could see was if less than 5 subjects are given for a student (even though question says at least 5) 
+
             }
-            catch (Exception)
-            {
+           catch (Exception)
+           {
 
                 throw;
-            }
+           }
         }
 
         //Question 8
@@ -330,15 +544,68 @@ namespace Assignment2_DIS_Spring2021
         {
             try
             {
-                //write your code here.
-                return false;
+
+                if (n < 0) // edge case if negative number is passed. 
+                {
+                    return false;
+                }
+
+                List<int> vistited = new List<int>();
+
+                while (n != 0)  // Loop till there is a number
+                {
+
+                    n = sumsquares(n); // Call the helper function to calculate sum of square of all digits
+
+                    if (n == 1)         // Case 1: if sum of square is 1
+                        return true;
+
+                    if (vistited.Contains(n))  // Has this number been seen before
+                    {
+                        return false;          // if it has been seen return true as we will go in loop. 
+
+                    } else
+                    {
+                        vistited.Add(n);        // if the number is not see add to list 
+                    }
+
+
+
+                }
+                return false;          // Return false for edge cases
+
             }
+
+        
             catch (Exception)
             {
 
                 throw;
             }
         }
+
+
+
+        // Helper function to calculate the sum of square all digits of a number
+        public static int sumsquares(int n)
+        {
+
+            int sq = 0;     //intialize the square sum 
+            while (n != 0)
+            {
+                sq = sq + Convert.ToInt32(Math.Pow(n % 10, 2));  // add square of digit to bymber
+                n = n / 10;   // got 1 digit back. 
+
+
+            }
+            return sq;   // return the sum 
+        }
+
+
+        // Reflection:  There was 2 issues here. 1. What to do if we come to the same number again what will happen. 2. Keep calculating sum of square of digits recursively.
+        // The solution was to have a list and keep track of all the numbers seen and to make a helper function which can be called recursively to calculate sum of square of digits.
+
+
 
         //Question 9
         /// <summary>
@@ -361,15 +628,30 @@ namespace Assignment2_DIS_Spring2021
         {
             try
             {
-                //write your code here.
-                return 0;
+                int minp = int.MaxValue;  // Define variable to keep track of min Price. Defining it as max int value. 
+                int maxp = 0;             // Variable to keep track of max profit
+
+                // Loop through the prices
+                for (int i = 0; i < prices.Length; i++)
+                {
+                    if (prices[i] < minp)    // keep track of min price
+                        minp = prices[i];
+                    else if (prices[i] - minp > maxp)  // calculate profit
+                        maxp = prices[i] - minp;      // track of max profit
+                }
+                return maxp;
+
             }
+
+            // Reflection: Noting that the array is a time array, that is, to find the best buying point and selling point was the key. 
             catch (Exception)
             {
 
                 throw;
             }
         }
+
+
 
         //Question 10
         /// <summary>
@@ -397,7 +679,30 @@ namespace Assignment2_DIS_Spring2021
             try
             {
                 //write your code here.
-              
+
+                // Handling edge cases
+                if (steps <= 0)
+                    Console.WriteLine("Need at least 1 step");
+                if (steps == 1)
+                    Console.WriteLine(1);
+
+                // Getting to main code. 
+                if (steps > 1)
+                {
+
+
+
+                    int first = 1;  // define variable 
+                    int second = 2; // define variable 
+                    for (int i = 3; i <= steps; i++)
+                    {
+                        int third = first + second; // calculate number of ways for the step i
+                        first = second;             // store values for next step 
+                        second = third;
+                    }
+                    Console.WriteLine(second);
+                }
+
             }
             catch (Exception)
             {
@@ -405,5 +710,8 @@ namespace Assignment2_DIS_Spring2021
                 throw;
             }
         }
+
+        // Reflection: Figuring out the logic to calculate the fibonacci number was tricky. Once I figured out that the number repeat and add, it was simpler.
+        //Also figuring out the right edge cases. 
     }
 }
